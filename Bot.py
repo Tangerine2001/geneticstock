e2246bc2cb0f3217b220a76a8f0931bc44944a82
Max Tang <maxtang2001@gmail.com>
2021-05-24 14:59:35 +0200

    added the bulk of the main program

--- Bot.py
+++ Bot.py
@@ -2,15 +2,15 @@ import random
 
 
 class Bot:
-    def __init__ (self, name, is_random, weights):
+    def __init__(self, name, is_random, weights):
         self.name = name
         self.weights = weights
         if not is_random:
-            self.weights += random.uniform(-1, 1)
+            for i in range(len(self.weights)):
+                self.weights[i] += random.uniform(-1, 1)
 
     def __eq__(self, other):
         return self.name == other.name
 
     def __repr__(self):
         return "Bot(Name: {})".format(self.name)
-
--- main.py
+++ main.py
@@ -1,7 +1,24 @@
+import random
+from copy import copy
 from Bot import Bot
 
+
 def main():
-    b = Bot("BooBoo")
-    botList = []
-    for i in range(500):
-        botList[i] = Bot("change this please")
+    bot_list = []
+    for i in range(5):
+        bot_list.append(Bot("change this please", True,
+                            [random.uniform(-10, 10), random.uniform(-10, 10), random.uniform(-10, 10),
+                             random.uniform(-10, 10), random.uniform(-10, 10)]))
+
+    print(bot_list[0].weights)
+    while True:
+        best_bots = [copy(bot_list[0]), copy(bot_list[1]), copy(bot_list[2]), copy(bot_list[3]), copy(bot_list[4])]
+        new_bot_list = []
+        for i in range(5):
+            new_bot_list.append(Bot("Howard", False, best_bots[i].weights))
+        break
+    print(new_bot_list[0].weights)
+
+
+if __name__ == '__main__':
+    main()
